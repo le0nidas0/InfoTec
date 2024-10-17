25c874e179f10f59e48ff8e02354f9356de2c61d
leonidas <leonidasmoreira098@gmail.com>
2024-10-17 20:51:52 -0300

    Estrutura do projeto

---
+++ InfoTec/src/test/java/com/example/InfoTec/InfoTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InfoTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
